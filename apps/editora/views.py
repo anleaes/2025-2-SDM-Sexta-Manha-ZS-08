affa23663cac75e1c9f2513a843c4ccc46dec3b4
jpgercc <jpgercc@gmail.com>
2025-11-25 20:52:01 -0300

    add

--- apps/editora/views.py
+++ apps/editora/views.py
@@ -3,3 +3,6 @@ from rest_framework import viewsets
 from editora.serializer import EditoraSerializer
 
 # Após o comentario "# Create your views here." e crie as views "Editora".
+class EditoraViewSet(viewsets.ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer  
