0d3264a86202e08336f71cdbf4aedefb74db04bd
jpgercc <jpgercc@gmail.com>
2025-11-25 17:40:03 -0300

    init

--- apps/biblioteca/models.py
+++ apps/biblioteca/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Biblioteca(models.Model):
+    nome = models.CharField('Nome completo', max_length=50)
+
+    class Meta:
+        verbose_name = 'Biblioteca'
+        verbose_name_plural = 'Bibliotecas'
+        ordering =['id']
+
+    def __str__(self):
+        return self.nome
