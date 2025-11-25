ca47e11a8068bfab6cc1b5b09acdd68af027a2d4
jpgercc <jpgercc@gmail.com>
2025-11-25 17:40:16 -0300

    add models

--- apps/biblioteca/models.py
+++ apps/biblioteca/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Biblioteca(models.Model):
     nome = models.CharField('Nome completo', max_length=50)
+    endereco = models.CharField('Endereço', max_length=200)
+    telefone = models.CharField('Telefone', max_length=20)
 
     class Meta:
         verbose_name = 'Biblioteca'
