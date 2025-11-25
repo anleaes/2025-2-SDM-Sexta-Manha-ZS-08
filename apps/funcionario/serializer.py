f8570ca8ac391cea86264401571d64b780e41fa7
jpgercc <jpgercc@gmail.com>
2025-11-25 16:12:11 -0300

    exlude

--- apps/funcionario/serializer.py
+++
@@ -1,7 +0,0 @@
-from .models import Funcionario
-from rest_framework import serializers
-
-class FuncionarioSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Funcionario
-        fields = '__all__'
