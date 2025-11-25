83647856f0e6568c9cb5f8efb3eb6cc63159f9a1
jpgercc <jpgercc@gmail.com>
2025-11-25 20:47:49 -0300

    1

--- apps/editora/models.py
+++ apps/editora/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Editora(models.Model):
+
+    class Meta:
+        verbose_name = 'Editora'
+        verbose_name_plural = 'Editoras'
+        ordering =['id']
+
+    def __str__(self):
+        return self.nome
