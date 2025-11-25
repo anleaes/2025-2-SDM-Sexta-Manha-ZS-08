d418286a70a55ee689eaaf64a03ca5b58f9beae6
jpgercc <jpgercc@gmail.com>
2025-11-25 17:46:33 -0300

    add views

--- apps/biblioteca/views.py
+++ apps/biblioteca/views.py
@@ -3,3 +3,7 @@ from biblioteca.models import Biblioteca
 from rest_framework import viewsets
 from biblioteca.serializer import BibliotecaSerializer
 
+# Create your views here.
+class BibliotecaViewSet(viewsets.ModelViewSet):
+    queryset = Biblioteca.objects.all()
+    serializer_class = BibliotecaSerializer  
