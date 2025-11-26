106f39d32129e197ebce428e260d2ea009f3d90b
jpgercc <jpgercc@gmail.com>
2025-11-25 21:43:00 -0300

    add

--- apps/funcionario/models.py
+++ apps/funcionario/models.py
@@ -4,7 +4,15 @@ from biblioteca.models import Biblioteca
 # Após o comentario "# Create your models here." e crie a classe "Order" do modelo.
 
 class Funcionario(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+    email = models.CharField('Email', max_length=50)
+    telefone = models.CharField('Telefone', max_length=15)
+    cargo = models.CharField('Cargo', max_length=100)
+    data_admissao = models.DateField('Data de Admissão')
+    ativo = models.BooleanField('Ativo', default=True)
 
+    biblioteca = models.ForeignKey(Biblioteca, on_delete=models.CASCADE, verbose_name='Biblioteca', null=True)
+    
     class Meta:
         verbose_name = 'Funcionario'
         verbose_name_plural = 'Funcionarios'
