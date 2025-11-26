c8959351fabe895ec13107e91cf1e7248344b1bb
jpgercc <jpgercc@gmail.com>
2025-11-25 22:11:56 -0300

    init

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+        ordering =['id']
+
+    def __str__(self):
+        return self.nome
