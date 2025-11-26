9535488fcc907c1b140b7e2c067ff6179cca94f8
jpgercc <jpgercc@gmail.com>
2025-11-25 21:42:51 -0300

    init

--- apps/funcionario/models.py
+++ apps/funcionario/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from biblioteca.models import Biblioteca
 
-# Create your models here.
+# Após o comentario "# Create your models here." e crie a classe "Order" do modelo.
+
+class Funcionario(models.Model):
+
+    class Meta:
+        verbose_name = 'Funcionario'
+        verbose_name_plural = 'Funcionarios'
+        ordering =['id']
+
+    def __str__(self):
+        return self.nome 
