98b994d05d900c66c4dcf482383598e9d872b1a5
jpgercc <jpgercc@gmail.com>
2025-11-25 21:49:24 -0300

    init

--- apps/funcionario/views.py
+++ apps/funcionario/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from funcionario.models import Funcionario
+from rest_framework import viewsets
+from funcionario.serializer import FuncionarioSerializer
 # Create your views here.
+
+
+# Após o comentario "# Create your views here." e crie as views "Funcionario".
+
