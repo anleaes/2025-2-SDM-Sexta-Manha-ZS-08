9852b0f5c4fad298b6b080f430780e78b82b1b12
jpgercc <jpgercc@gmail.com>
2025-11-25 22:03:03 -0300

    init

--- apps/livro/views.py
+++ apps/livro/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from livro.models import Livro
+from livro.serializer import LivroSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
+# Após o comentario "# Create your views here." e crie as views "Category".
+
