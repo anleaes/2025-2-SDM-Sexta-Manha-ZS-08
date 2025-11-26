dad483032d4a3aece3295eb68b441f31c1e9df5b
jpgercc <jpgercc@gmail.com>
2025-11-25 21:59:42 -0300

    init

--- apps/livro/models.py
+++ apps/livro/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+#from products.models import Product
+#from orders.models import Order
+from autor.models import Autor
+from editora.models import Editora
+#from exemplar.models import Exemplar
 # Create your models here.
+
+# Após o comentario "# Create your models here." e crie a classe "Livro" do modelo.
+
+class Livro(models.Model):
+
+    class Meta:
+        verbose_name = 'Livro'
+        verbose_name_plural = 'Livros'
+        ordering =['id']
+
+    def __str__(self):
+        return f"{self.title} - {self.autor} - {self.editora}"
