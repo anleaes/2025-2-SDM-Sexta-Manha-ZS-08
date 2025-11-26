2f837187d61f4801c86797f6a92fdd662f930716
jpgercc <jpgercc@gmail.com>
2025-11-25 22:16:37 -0300

    add

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -6,8 +6,3 @@ class UsuarioSerializer(serializers.ModelSerializer):
         model = Usuario
         fields = '__all__'
         
-        # Para chamar todos os atributos:
-        # fields = '__all__'
-        
-        # Para chamar somentes os atributos de interesse:
-        # fields = ['id','created_on', 'updated_on', 'name', 'description']
