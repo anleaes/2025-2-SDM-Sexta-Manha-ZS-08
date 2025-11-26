30a24284cbbb89f9d8d581173bf4f1625d4729d5
jpgercc <jpgercc@gmail.com>
2025-11-25 22:03:16 -0300

    add

--- apps/livro/views.py
+++ apps/livro/views.py
@@ -7,3 +7,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 # Create your views here.
 # Após o comentario "# Create your views here." e crie as views "Category".
 
+class LivroViewSet(viewsets.ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'autor', 'editora', 'ano_publicacao'] 
