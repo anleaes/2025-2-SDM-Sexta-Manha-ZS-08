905ef5f697a1091c4ae6a9aa2f4b2caeab089284
jpgercc <jpgercc@gmail.com>
2025-11-25 21:23:29 -0300

    add

--- apps/exemplar/models.py
+++ apps/exemplar/models.py
@@ -5,7 +5,17 @@ from livro.models import Livro
 
 # Após o comentario "# Create your models here." e crie a classe "Exemplar" do modelo.
 class Exemplar(models.Model):
-
+    codigo_de_barras = models.CharField(max_length=100, unique=True)
+    data_aquisicao = models.DateField('Data de Aquisição')
+    estado_de_convervacao = models.CharField('Estado de Conservação', max_length=100)
+    preco_de_compra = models.DecimalField('Preço de Compra', max_digits=10, decimal_places=2)
+    localizacao = models.CharField('Localização', max_length=100)
+    disponivel = models.BooleanField('Disponível', default=True)
+    observacoes = models.TextField('Observações', blank=True, null=True)
+    foto_capa = models.ImageField(upload_to='exemplar_covers/', blank=True, null=True, verbose_name='Foto da Capa') #extra/ fora do escopo da modelagem
+    
+    biblioteca = models.ForeignKey(Biblioteca, on_delete=models.CASCADE, verbose_name='Biblioteca')
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE, verbose_name='Livro', null=True)
     class Meta:
         verbose_name = 'Exemplar'
         verbose_name_plural = 'Exemplares'
